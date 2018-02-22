629e3efde0477d9638f57bf7442524947152506c
Lee Caninzun <lee.caninzun@arcadiasolutions.com>
2018-02-22 14:29:57 -0500

    [delete] restructure the test directory 	 move all current tests under the spec dir 	 remove reference to unit_tests for now

--- test/spec/spec_helper.rb
+++
@@ -1,7 +0,0 @@
-PROJECT_ROOT ||= File.expand_path(File.join(File.dirname(__FILE__), '..')).freeze
-LIB_LOCATION = File.join(PROJECT_ROOT, 'lib')
-$LOAD_PATH << LIB_LOCATION
-# This is a file that contains all the commonly used test methods
-require 'dotenv/load' # to load native .env file
-require 'pry'
-require 'pry-nav' # allows next, step, keywords when debugging
